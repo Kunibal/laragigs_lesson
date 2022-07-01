4814fba0c0bad0195d27622b6fb0c95458fc2dd3
Alexander Kun <alexinyamagata@gmail.com>
2022-07-01 14:50:22 +0900

    add components for a single listing

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Latest Listings',
         'listings' => Listing::all()
     ]);
 });
 
-Route::get('/listings/{id}', function ($id) {
+Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
